782ff5a03e543da17e8bbd347e15dbc41ec8b196
tsimashevick <atsimashevich@elinext.com>
2023-11-10 17:06:08 +0300

    Added categories and meals dumb data.

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({
+    super.key,
+    required this.meal,
+  });
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
