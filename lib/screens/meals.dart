62f89cb0e97d3762ac29f7d8dad9f1ae7f72774f
tsimashevick <atsimashevich@elinext.com>
2023-11-13 13:27:21 +0300

    Added tabs screen.

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -5,9 +5,9 @@ import 'package:meals/widgets/meal_item.dart';
 import '../models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -52,9 +52,12 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if (title == null) {
+      return content;
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
